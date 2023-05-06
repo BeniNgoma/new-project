62f221df9e79fd3b4ee089464c95c05bc694950f
BeniNgoma <beningoma244@gmail.com>
2023-05-07 00:47:06 +0200

    ajout des fichiers au dépôt

---
+++ page.js
@@ -0,0 +1,17 @@
+let changecolor = document.getElementById("ben")
+function chcolor(newcolor){
+    changecolor.classList.add("ben")
+    changecolor.style.backgroundColor = newcolor;
+}
+let displaylet = document.getElementById("ben")
+function showdiv(){
+
+     displaylet.classList.add("ben")
+   }
+   let hidedisplay = document.getElementById("ben")
+   function hidediv(){
+       displaylet.classList.remove("ben")
+    
+   
+           }
+
